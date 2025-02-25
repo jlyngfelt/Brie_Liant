43462cd102f212c6c45cb72357abbb246304e323
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-02-25 16:01:11 +0100

    creatded route to products.index

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
-    return view('index');
+    return view('home');
 });
 
-Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products', [ProductController::class, 'index'])
+->name('products.index');
 
