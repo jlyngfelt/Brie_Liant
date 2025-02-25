72216abd8320148b1854193cfc5e1b3d375c0e72
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-02-25 16:56:32 +0100

    Updated ProductController

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,3 +1,12 @@
 <x-layout>
     <h1>Products</h1>
+
+    @foreach($products as $product)
+        <h2>{{ $product->name }}</h2>
+        <p>{{ $product->description }}</p>
+        <p>{{ $product->price }}</p>
+        <img src="{{ $product->image_path }}" alt="Ostbricka">
+    @endforeach
+
 </x-layout>
+
