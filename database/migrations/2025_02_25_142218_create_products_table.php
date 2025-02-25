8381671d85c59decd3b9a473956e50eb3f9d4050
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-02-25 15:41:13 +0100

    small change

--- database/migrations/2025_02_25_142218_create_products_table.php
+++ database/migrations/2025_02_25_142218_create_products_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->decimal('price', 8, 2);
             $table->string('image_path')->nullable();
             $table->timestamps();
+            
         });
     }
 
