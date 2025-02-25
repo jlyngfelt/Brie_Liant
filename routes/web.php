9a507f9d46ea51ffa48a183f2d7da9b22ac6d0ad
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-02-25 15:50:25 +0100

    made product controller, route and view work together

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/products', [ProductController::class, 'index']);
 
