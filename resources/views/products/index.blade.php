a076a9ae47e4fab144247db72584ef90aac61f59
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-02-26 10:11:30 +0100

    Added link to the new product page

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,6 +1,8 @@
 <x-layout>
     <h1>Products</h1>
 
+<a href="{{ route('products.create') }}">New Product</a>
+
     @foreach($products as $product)
         <h2>{{ $product->name }}</h2>
         <p>{{ $product->description }}</p>
