8138a88541400da7f3686ad07815521c698ff7dc
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-02-26 12:06:24 +0100

    Added logic to fix if $product is empty

--- resources/views/components/product/form.blade.php
+++ resources/views/components/product/form.blade.php
@@ -1,13 +1,13 @@
 @csrf
 
 <label for="name">Name</label>
-<input type="text" name="name" id="name" value="{{ old('name', $product->name) }}">
+<input type="text" name="name" id="name" value="{{ old('name', $product->name ?? '') }}">
 
 <label for="description">Description</label>
-<textarea name="description" id="description">{{ old('description', $product->description) }}</textarea>
+<textarea name="description" id="description">{{ old('description', $product->description ?? '') }}</textarea>
 
 <label for="price">Price</label>
-<input type="number" name="price" id="price" step="1.0" value="{{ old('price', $product->price) }}">
+<input type="number" name="price" id="price" step="1.0" value="{{ old('price', $product->price ?? '') }}">
 
 <label for="image_path">Upload Image</label>
 <input type="text" name="image_path" id="image_path" >
