898bcb026d4b5bfc872e78b48844a3cecff2ec02
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-02-26 10:23:21 +0100

    added route

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,6 @@ Route::get('/products', [ProductController::class, 'index'])
 
 Route::get('/products/create', [ProductController::class, 'create'])
     ->name('products.create');
+
+Route::post('/products/store', [ProductController::class, 'store'])
+    ->name('products.store');
