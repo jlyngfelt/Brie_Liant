6a3af9c74f7b2a36e0952ba762cf424cd214ed91
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-03-05 15:07:50 +0100

    -

--- lang/sv/validation.php
+++ lang/sv/validation.php
@@ -152,12 +152,10 @@ return [
     'uppercase'              => ':Attribute måste vara versaler.',
     'url'                    => ':Attribute har ett ogiltigt format.',
     'uuid'                   => ':Attribute måste vara ett giltigt UUID.',
-
-        'attributes' => [
-            'name' => 'Namn',
-            'description' => 'Beskrivning',
-            'price' => 'Pris',
-            'category_id' => 'Osttyp',
-        
-    ]
+    'attributes'             => [
+        'category_id' => 'Osttyp',
+        'description' => 'Beskrivning',
+        'name'        => 'Namn',
+        'price'       => 'Pris',
+    ],
 ];
