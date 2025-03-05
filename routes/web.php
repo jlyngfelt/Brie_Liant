6e91c82c62c3a8b7d407b8975afaae989af1c202
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-03-05 15:47:26 +0100

    Removed old routes

--- routes/web.php
+++ routes/web.php
@@ -9,29 +9,14 @@ require __DIR__ . '/auth.php';
 
 Route::get('/', function () {
     if (Auth::check()) {
-        return redirect()->route('products.index'); // Om inloggad, skicka till products
+        return redirect()->route('products.index'); // If logged in, redirect to products
     } else {
-        return redirect()->route('login'); // Om inte inloggad, skicka till login
+        return redirect()->route('login'); // If not logged in, redirect to login
     }
 });
 
-// Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-
-// Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
-
-// Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-// Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
-
-// Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-// Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
-// Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-// Route::patch('/products/{product}', [ProductController::class, 'update'])->name('products.update');
-// Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
-
-
 Route::middleware(['auth'])->group(function () {
     Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-    Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
 
     Route::middleware(['admin'])->group(function () {
         Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
@@ -40,4 +25,6 @@ Route::middleware(['auth'])->group(function () {
         Route::patch('/products/{product}', [ProductController::class, 'update'])->name('products.update');
         Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
     });
+
+    Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
 });
